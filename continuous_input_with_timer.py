dc3a3dd116c26430828bd329dcfe40a3deb71f55
LeenKharboutli <leen.kharboutli@protonmail.com>
2025-09-20 10:50:31 -0400

    zetamac like game is correctly playable now, next step is gui

--- continuous_input_with_timer.py
+++ continuous_input_with_timer.py
@@ -38,6 +38,7 @@ import threading
 from time import time
 import datetime
 from random import randint
+import argparse 
 
 multiplication_factor_1_range = (2, 12)
 multiplication_factor_2_range = (2, 100)
@@ -62,6 +63,7 @@ class KeyboardThread(threading.Thread):
     def __init__(self, input_cbk = None, name='keyboard-input-thread'):
         self.input_cbk = input_cbk
         self.user_ans = None
+        self.score = 0
         super(KeyboardThread, self).__init__(name=name, daemon=True)
         self.start()
 
@@ -70,10 +72,17 @@ class KeyboardThread(threading.Thread):
             factor_1, factor_2, ans = generate_multiplication_problem()
             print(f"{factor_1} x {factor_2} = ")
             user_ans = self.input_cbk(input()) #waits to get input + Return
-            if user_ans != str(ans):
-                print('wrong!')
-            else:
+            while user_ans != str(ans) and time() < end_time:
+                print('wrong! Try again:')
+                user_ans = self.input_cbk(input())
+            if user_ans == str(ans):
                 print('correct')
+                self.score += 1
+                print('Score:', self.score)
+            # if user_ans != str(ans):
+            #     print('wrong!')
+            # else:
+            #     print('correct')
 
 showcounter = 0 #something to demonstrate the change
 
@@ -88,15 +97,25 @@ def my_callback(inp):
     return inp
         # pass
 
-#start the Keyboard thread
-kthread = KeyboardThread(my_callback)
 
-while True:
-    #the normal program executes without blocking. here just counting up
-    # print(kthread.user_ans) # Tried to see if there was a way to access the input from the callback
-    if time() >= end_time:
-        print(datetime.datetime.fromtimestamp(time()))
-        print("Game over!")
-        
-        break
-    # showcounter += 1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Zetamac-like timed mental math game.')
+    parser.add_argument('--time', type=int, default=120, help='Enter game length in seconds.')
+
+    args = parser.parse_args()
+    print(args.time)
+
+    #start the Keyboard thread
+    kthread = KeyboardThread(my_callback)
+
+    while True:
+        #the normal program executes without blocking. here just counting up
+        # print(kthread.user_ans) # Tried to see if there was a way to access the input from the callback
+        if time() >= end_time:
+            print(datetime.datetime.fromtimestamp(time()))
+            print("Game over!")
+            print('Final Score:', kthread.score)
+            break
+        # showcounter += 1
